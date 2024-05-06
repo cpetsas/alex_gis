c493b2fe0a2c19097a1b12606486518dec0e25dc
Charis Petsas <charis.petsas96@gmail.com>
2024-05-06 17:39:36 +0300

    final BE touches

--- backend/article_api/article_api/controllers/auth_controller.py
+++ backend/article_api/article_api/controllers/auth_controller.py
@@ -44,25 +44,3 @@ class AuthorisationController(BaseController):
             return JsonResponse({"message": "User not found"}, status=404)
         return JsonResponse({"user_token": jwt_token}, status=200)
 
-    # def login(self, username, password):
-    #     user_exists = User.objects.filter(username=username, password=password).exists()
-    #     if user_exists:
-    #         user = User.objects.get(username=username)
-    #         tidy_user = self.clean_object(user, "username")
-    #     else:
-    #         return JsonResponse({"message": "User not found"}, status=404)
-    #     return JsonResponse({"user": tidy_user}, status=200)
-
-
-# auth_controller = AuthorisationController()
-# @csrf_exempt
-# @require_http_methods(['POST'])
-# def login(request):
-#     request_body = json.loads(request.body)
-#     try:
-#         username = request_body["username"]
-#         password = request_body["password"]
-#     except:
-#         return JsonResponse({"message": "Please provide username and password"}, status=400)
-    
-#     return auth_controller.login(username, password)
